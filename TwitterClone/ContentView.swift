a06d57b6f2e1e319f246a255c5f5679cf647bd60
이상준 <>
2024-07-07 23:00:44 +0900

    init: 프로젝트 세팅

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by 이상준 on 7/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by 이상준 on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
