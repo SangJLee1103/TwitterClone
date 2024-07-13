2134b33e1a1cb4adb432ea7dd46007fa994e6246
이상준 <>
2024-07-13 20:26:32 +0900

    feat: 피드 뷰 플로팅 버튼 추가

---
+++ TwitterClone/View/FeedView.swift
@@ -0,0 +1,34 @@
+//
+//  FeedView.swift
+//  TwitterClone
+//
+//  Created by 이상준 on 7/13/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                
+            }
+            
+            Button(action: {}, label: {
+                Image("tweet")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color(.systemBlue))
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
