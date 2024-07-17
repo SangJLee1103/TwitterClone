03d216b3dbe7d36d2419266d5bec5405f7241912
이상준 <>
2024-07-17 22:48:52 +0900

    feat: Message Bubble UI 구현 및 Mock 데이터 추가

--- TwitterClone/View/Messages/ChatView.swift
+++ TwitterClone/View/Messages/ChatView.swift
@@ -14,19 +14,11 @@ struct ChatView: View {
         VStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 12) {
-                    ForEach(0..<15) { _ in
-                        HStack {
-                            Spacer()
-                            Text("Test message text")
-                                .padding()
-                                .background(.blue)
-                                .clipShape(ChatBubble(isFromCurrentUser: true))
-                                .foregroundStyle(.white)
-                                .padding(.horizontal)
-                        }
+                    ForEach(MOCK_MESSAGES) { message in
+                        MessageView(message: message)
                     }
                 }
-            }
+            }.padding(.top)
             
             MessageInputView(messageText: $messageText)
                 .padding()
