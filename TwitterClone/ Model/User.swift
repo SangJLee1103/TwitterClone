60def1bfe543917b3c42382ef6afd39fe645031e
이상준 <>
2024-07-24 18:18:06 +0900

    feat: 유저 조회 구현

---
+++ TwitterClone/ Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  TwitterClone
+//
+//  Created by 이상준 on 7/24/24.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    let id: String
+    let username: String
+    let profileImageUrl: String
+    let fullname: String
+    let email: String
+    
+    init(dictionary: [String: Any]) {
+        self.id = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.fullname = dictionary["fullname"] as? String ?? ""
+    }
+}
