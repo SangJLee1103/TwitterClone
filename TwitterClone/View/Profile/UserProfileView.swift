b5506217e1f29238313f0643f4a62984ae2c9bf3
이상준 <>
2024-07-31 22:48:08 +0900

    feat: Follow 기능 구현

--- TwitterClone/View/Profile/UserProfileView.swift
+++ TwitterClone/View/Profile/UserProfileView.swift
@@ -8,26 +8,26 @@
 import SwiftUI
 
 struct UserProfileView: View {
-    @State var selectedFilter: TweetFilterOptions = .tweets
+    let user: User
+    @ObservedObject var viewModel: ProfileViewModel
+    
+    init(user: User) {
+        self.user = user
+        self.viewModel = ProfileViewModel(user: user)
+    }
     
     var body: some View {
         ScrollView {
             VStack {
-                ProfileHeaderView()
+                ProfileHeaderView(isFollowed: $viewModel.isFollowed, viewModel: viewModel)
                     .padding()
                 
-                FilterButtonView(selectedOption: $selectedFilter)
-                    .padding()
-                
-                ForEach(0..<9) { tweet in
+                ForEach(0..<5) { tweet in
                     TweetCell()
+                        .padding()
                 }
             }
             .navigationTitle("batman")
         }
     }
-}
-
-#Preview {
-    UserProfileView()
 }
